b9b357416209548349845c753c63edfa7b6b5e08
MarSury03 <sm.torres@alum.up.edu.pe>
2025-11-04 22:57:46 -0500

    Add files via upload

---
+++ app (2).py
@@ -0,0 +1,6 @@
+
+import streamlit as st
+
+st.title("Hola desde Docker y Streamlit 👋")
+st.write("Esta es una aplicación simple creada en Colab y desplegada con Docker.")
+st.write("Autor: Tu Nombre")
